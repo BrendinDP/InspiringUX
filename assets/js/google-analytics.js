c002bb229ecbf65e10b6271de8e395c92f2eaf49
Brendin du Plessis <brendin@bazaruto.com>
2018-02-20 17:13:17 +0200

    first commit

---
+++ assets/js/google-analytics.js
@@ -0,0 +1,8 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-114354084-1');
+/**
+ * Created by brendindp on 2/20/18.
+ */
